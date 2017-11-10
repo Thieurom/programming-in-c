5809d54d4179212e8253da76da52bb74baf0c812
Doan Le Thieu <thieurom@gmail.com>
2017-11-10 23:52:40 +0700

    add exercise 3.5

---
+++ 03-varibles-types/exercises/ex_03_05.c
@@ -0,0 +1,18 @@
+// 
+// Exercise 3.5
+//
+// Doan Thieu - 10/2017.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    char c, d;
+
+    c = 'd';
+    d = c;
+    printf("d = %c\n", d);  // output: d = d
+
+    return 0;
+}
