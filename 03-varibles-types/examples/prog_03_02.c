bde35da457bed75900acdc8a819e0fdaa72eab15
Doan Le Thieu <thieurom@gmail.com>
2017-11-10 20:55:33 +0700

    add chapter 3's examples

---
+++ 03-varibles-types/examples/prog_03_02.c
@@ -0,0 +1,35 @@
+//
+// Program 3.2
+// Using the Arithmetic Operators
+//
+// Code example from the book: Programming in C (4th Edition) by Stephen G. Kochan
+//
+
+// Illustrate the use of various arithmetic operators
+
+#include <stdio.h>
+
+int main (void)
+{
+    int a = 100;
+    int b = 2;
+    int c = 25;
+    int d = 4;
+    int result;
+
+    result = a - b;       // substraction
+    printf ("a - b = %i\n", result);
+
+    result = b * c;       // multiplication
+    printf ("b * c = %i\n", result);
+
+    result = a / c;       // division
+    printf ("a / c = %i\n", result);
+
+    result = a + b * c;   // precedence
+    printf ("a + b * c = %i\n", result);
+
+    printf ("a * b + c * d = %i\n", a * b + c * d);
+
+    return 0;
+}
