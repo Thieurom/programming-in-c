489931d0576d674980c6c18b8372ea758c08a86b
Doan Le Thieu <thieurom@gmail.com>
2017-11-10 15:30:01 +0700

    add exercise 2.4

---
+++ 02-first-program/exercises/ex_02_04.c
@@ -0,0 +1,19 @@
+// 
+// Exercise 2.4
+//
+// Doan Thieu - 10/2017.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int value1, value2, diff;
+
+    value1 = 87;
+    value2 = 15;
+    diff = value1 - value2;
+    printf("%i minus %i equals %i.", value1, value2, diff);
+
+    return 0;
+}
