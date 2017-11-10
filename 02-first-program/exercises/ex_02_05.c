c194bfd525cbd7c5e852323f6401d401144c12ff
Doan Le Thieu <thieurom@gmail.com>
2017-11-10 15:39:22 +0700

    add exercise 2.5 ~> complete chapter 2

---
+++ 02-first-program/exercises/ex_02_05.c
@@ -0,0 +1,24 @@
+// 
+// Exercise 2.5
+//
+// Doan Thieu - 10/2017.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int value1, value2, value3, result;
+
+    value1 = 25;
+    value2 = 37;
+    value3 = 19;
+
+    // Compute result
+    result = value1 + value2 - value3;
+
+    // Display result
+    printf("The answer is %i\n", result);
+
+    return 0;
+}
