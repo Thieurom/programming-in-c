9d4304617c39fcbb5af42bdafade2d44c538c589
Doan Le Thieu <thieurom@gmail.com>
2017-11-11 23:05:21 +0700

    add exercise 4.7

---
+++ 04-program-looping/exercises/ex_04_07.c
@@ -0,0 +1,22 @@
+// 
+// Exercise 4.7
+//
+// Doan Thieu - 11/2017.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int dollars, cents, count;
+
+    for (count = 1; count <= 10; ++count) {
+        printf("Enter dollars: ");
+        scanf("%i", &dollars);
+        printf("Enter cents: ");
+        scanf("%i", &cents);
+        printf("$%i.%.2i\n\n", dollars, cents);
+    }
+
+    return 0;
+}
