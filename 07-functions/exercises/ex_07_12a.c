b951f0c6d543a6f1a7fdf63eacbe86dc6b96b5fa
Doan Le Thieu <thieurom@gmail.com>
2017-11-16 21:02:40 +0700

    add exercises 7.12a, 7.12b

---
+++ 07-functions/exercises/ex_07_12a.c
@@ -0,0 +1,54 @@
+// 
+// Exercise 7.12a
+//
+// Doan Thieu - 11/2017.
+//
+
+#include <stdio.h>
+
+int result[5][4];
+
+int main(void)
+{
+    void transpose_matrix(int matrix[4][5]);
+    void display_matrix(int n_rows, int n_cols, int matrix[n_rows][n_cols]);
+    int sample_matrix[4][5] = {
+        { 10,  5, -3, 17, 12 },
+        { 12, 10, 52,  0,  7 },
+        { -2,  1,  2,  4,  9 },
+        {  0,  0,  8,  7,  6 }
+    };
+
+    printf("Original matrix:\n");
+    display_matrix(4, 5, sample_matrix);
+
+    transpose_matrix(sample_matrix);
+
+    printf("\nTransposed matrix:\n");
+    display_matrix(5, 4, result);
+
+    return 0;
+}
+
+
+void transpose_matrix(int matrix[4][5])
+{
+    int i, j;
+
+    for (i = 0; i < 5; ++i)
+        for (j = 0; j < 4; ++j)
+            result[i][j] = matrix[j][i];
+}
+
+
+void display_matrix(int n_rows, int n_cols, int matrix[n_rows][n_cols])
+{
+    int i, j;
+
+    for (i = 0; i < n_rows; ++i) {
+        for (j = 0; j < n_cols; ++j)
+            printf("%5i", matrix[i][j]);
+
+        printf("\n");
+    }
+}
