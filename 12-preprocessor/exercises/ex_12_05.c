66d1766b1a32d9695dc8329efde2136d984633b3
Doan Le Thieu <thieurom@gmail.com>
2017-12-13 23:15:18 +0700

    add exercise 12.5

---
+++ 12-preprocessor/exercises/ex_12_05.c
@@ -0,0 +1,22 @@
+// 
+// Exercise 12.5
+//
+// Doan Thieu - 12/2017.
+//
+
+
+#include <stdio.h>
+
+#define SHIFT(value, n) ((n) > 0 ? ((value) << (n)) : ((value) >> -(n)))
+
+int main()
+{
+    unsigned int w1 = 0177777u, w2 = 0444u;
+
+    printf("%o\t%o\n", SHIFT(w1, 5), w1 << 5);
+    printf("%o\t%o\n", SHIFT(w1, -6), w1 >> 6);
+    printf("%o\t%o\n", SHIFT(w2, 0), w2 >> 0);
+    printf("%o\n", SHIFT(SHIFT(w1, -3), 3));
+
+    return 0;
+}
